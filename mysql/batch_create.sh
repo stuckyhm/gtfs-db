ba2d13fa7a54048cedfc14301f604e9e6dcbbc84
Sebastian Stuckenbrock <sstuckenbrock@efhm.de>
2014-12-28 22:38:49 +0200

    add batch create script

---
+++ mysql/batch_create.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ex
+
+MYSQL_HOST=127.0.0.1
+MYSQL_USER=root
+MYSQL_PWD=Asdf1234
+MYSQL_DB=gtfs
+
+
+MYSQL="mysql -vv -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PWD}"
+
+${MYSQL} -e "DROP DATABASE IF EXISTS ${MYSQL_DB};"
+${MYSQL} -e "CREATE DATABASE ${MYSQL_DB};"
+${MYSQL} ${MYSQL_DB} < tables_create.sql
+
+./load_feed.sh -h ${MYSQL_HOST} -u ${MYSQL_USER} -p ${MYSQL_PWD} -d ${MYSQL_DB} -f ../../nyc_subway_network/GTFS_nyc_Subway -w
+
+${MYSQL} ${MYSQL_DB} < generate_xtra_dates.sql
+${MYSQL} ${MYSQL_DB} < generate_xtra_service_dates.sql
+${MYSQL} ${MYSQL_DB} < generate_xtra_trips_extended.sql
+${MYSQL} ${MYSQL_DB} < generate_xtra_trip_stop_times.sql
+
