60e3c10b6b4dcbe48bd44bc6d0ae8af5c670f96a
Sebastian Stuckenbrock <sstuckenbrock@efhm.de>
2015-01-01 16:01:28 +0200

    move feed to var

--- mysql/batch_create.sh
+++ mysql/batch_create.sh
@@ -5,15 +5,17 @@ MYSQL_HOST=127.0.0.1
 MYSQL_USER=root
 MYSQL_PWD=Asdf1234
 MYSQL_DB=gtfs
+FEED=../../nyc_subway_network/GTFS_nyc_Subway
 
 
 MYSQL="mysql -vv -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PWD}"
 
 ${MYSQL} -e "DROP DATABASE IF EXISTS ${MYSQL_DB};"
 ${MYSQL} -e "CREATE DATABASE ${MYSQL_DB};"
+
 ${MYSQL} ${MYSQL_DB} < tables_create.sql
 
-./load_feed.sh -h ${MYSQL_HOST} -u ${MYSQL_USER} -p ${MYSQL_PWD} -d ${MYSQL_DB} -f ../../nyc_subway_network/GTFS_nyc_Subway -w
+./load_feed.sh -h ${MYSQL_HOST} -u ${MYSQL_USER} -p ${MYSQL_PWD} -d ${MYSQL_DB} -f ${FEED} -w
 
 ${MYSQL} ${MYSQL_DB} < generate_xtra_dates.sql
 ${MYSQL} ${MYSQL_DB} < generate_xtra_service_dates.sql
